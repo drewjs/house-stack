96ea60124290eea5da2bfbd85b19d4f967a01da9
Andrew Galvan <drewjsdev@pm.me>
2024-05-13 19:31:57 -0700

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,9 @@ export default {
   out: './migrations',
   schema: './app/db/schema.ts',
   breakpoints: true,
-  driver: 'turso',
+  dialect: 'sqlite',
+  // Set driver only if you are using aws-data-api, turso, d1-http, or expo
+  // driver: 'turso',
   dbCredentials: {
     url: process.env.DATABASE_URL,
     authToken: process.env.DATABASE_AUTH_TOKEN,
